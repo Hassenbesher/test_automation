37a9628bafb77c4eb37f36ef1557d30f978bdb70
Hassen <hassenbesher@gmail.com>
2016-05-22 14:55:10 -0700

    added email test scripts

---
+++ tests/email_test.js
@@ -0,0 +1,22 @@
+var email = require("../crede").email
+var password=require("../crede").password
+
+
+
+module.exports = {
+  'email_test' : function (browser) {
+    browser
+      .url("https://www.google.com/")
+      .waitForElementVisible('body', 1000)
+      .click("a[id=gb_70]")
+      .setValue("input[id=Email]", email)
+      .click("input[id=next]")
+      .pause(1000)
+      .setValue("input[id=Passwd]", password)
+      .click("input[id=signIn]")
+
+
+
+
+    }
+  };
